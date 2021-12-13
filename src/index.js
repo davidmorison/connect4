f7f1da29924004c54d91ae5193511658b5fe51b3
David Morison <nosiromdivad@gmail.com>
2021-12-13 09:52:14 -0700

    PvP works

--- src/index.js
+++ src/index.js
@@ -23,21 +23,12 @@ class Board extends React.Component {
   render() {
     return (
       <div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
+        <div className="board-row"> {this.renderSquare( 0)} {this.renderSquare( 1)} {this.renderSquare( 2)} {this.renderSquare( 3)} {this.renderSquare( 4)} {this.renderSquare( 5)} {this.renderSquare( 6)} </div>
+        <div className="board-row"> {this.renderSquare( 7)} {this.renderSquare( 8)} {this.renderSquare( 9)} {this.renderSquare(10)} {this.renderSquare(11)} {this.renderSquare(12)} {this.renderSquare(13)} </div>
+        <div className="board-row"> {this.renderSquare(14)} {this.renderSquare(15)} {this.renderSquare(16)} {this.renderSquare(17)} {this.renderSquare(18)} {this.renderSquare(19)} {this.renderSquare(20)} </div>
+        <div className="board-row"> {this.renderSquare(21)} {this.renderSquare(22)} {this.renderSquare(23)} {this.renderSquare(24)} {this.renderSquare(25)} {this.renderSquare(26)} {this.renderSquare(27)} </div>
+        <div className="board-row"> {this.renderSquare(28)} {this.renderSquare(29)} {this.renderSquare(30)} {this.renderSquare(31)} {this.renderSquare(32)} {this.renderSquare(33)} {this.renderSquare(34)} </div>
+        <div className="board-row"> {this.renderSquare(35)} {this.renderSquare(36)} {this.renderSquare(37)} {this.renderSquare(38)} {this.renderSquare(39)} {this.renderSquare(40)} {this.renderSquare(41)} </div>
       </div>
     );
   }
@@ -49,14 +40,13 @@ class Game extends React.Component {
     this.state = {
       history: [
         {
-          squares: Array(9).fill(null)
+          squares: Array(42).fill(null)
         }
       ],
       stepNumber: 0,
       xIsNext: true
     };
   }
-
   handleClick(i) {
     const history = this.state.history.slice(0, this.state.stepNumber + 1);
     const current = history[history.length - 1];
@@ -83,6 +73,25 @@ class Game extends React.Component {
   }
 
 
+  handleDrop(i){
+    // which is best "let", "const", "var" or nothing?
+    var nCol=7;
+    var nRow=6;
+    var inCol=i%nCol;
+    //var inRow=(i-inCol)/nCol;
+    //var outCol=inCol;
+    const history = this.state.history.slice(0, this.state.stepNumber + 1);
+    const current = history[history.length - 1];
+    const squares = current.squares.slice();
+    for (var outRow = nRow-1; outRow >= 0; outRow--){
+        var j=outRow*nCol+inCol;
+        if (!squares[j]){
+          this.handleClick(j);
+          return
+        }
+    }
+    //this.handleClick(i);
+  }
 
   render() {
   const history = this.state.history;
@@ -112,7 +121,7 @@ class Game extends React.Component {
         <div className="game-board">
           <Board
             squares={current.squares}
-            onClick={(i) => this.handleClick(i)}
+            onClick={(i) => this.handleDrop(i)}
           />
         </div>
         <div className="game-info">
