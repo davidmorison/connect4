56284666c68c627facd41b35d618120e044417e2
David Morison <nosiromdivad@gmail.com>
2021-12-14 15:25:19 -0700

    missing files?

--- py_backend/calcWinner.py
+++ py_backend/calcWinner.py
@@ -1,4 +1,37 @@
-def calcWinner(nrow,ncol,board,ind):
-    aWinner=False
-    aLine=[]
-    return aWinner, aLine
+from numpy import *
+def calcWinner(board,row,col,nrow,ncol):
+    dir_key=array([[ 1, 0],
+                   [ 1, 1],
+                   [ 0, 1],
+                   [-1,-1]])
+    steps=arange(4)
+    aWin=False
+    for i in range(4):
+        for j in range(4):
+            row4=dir_key[i,0]*(steps-j)+row
+            col4=dir_key[i,1]*(steps-j)+col
+            if min((row4)>=0) & (max(row4)<nrow) & (min(col4)>=0) & (max(col4)<ncol):
+                if all(board[row4[:],col4[:]]==board[row,col]):
+                    aWin=True
+                    return aWin, row4,col4
+    return aWin, [], []
+
+if __name__=='__main__':
+    board=array([[0, 0, 0, 0, 0, 0, 0],
+                 [0, 0, 0, 0, 0, 0, 0],
+                 [2, 0, 1, 0, 0, 0, 0],
+                 [1, 0, 1, 0, 0, 2, 2],
+                 [1, 1, 2, 2, 2, 2, 1],
+                 [2, 2, 1, 1, 2, 1, 1]])
+    row=4
+    col=3
+    nrow=6
+    ncol=7
+    aWin,win_row,win_col=calcWinner(board,row,col,nrow,ncol)
+    print(board)
+    board[win_row[:],win_col[:]]=4
+    board[row,col]=5
+    print(aWin)
+    print(win_col)
+    print(win_row)
+    print(board)
--- py_backend/train_connect4.py
+++ py_backend/train_connect4.py
@@ -1,40 +1,44 @@
 from numpy import *
 from matplotlib.pyplot import *
-#from calcWinner import calcWinner, sub2ind
-def sub2ind(i,j,nrow,ncol):
-    # I know nrow is not used, but this is easy to remember.
-    # could error check?
-    return j+i*ncol
-def calcWinner(board,row,col,nrow,ncol):
-    dir_key=array([[ 1, 0],
-                   [ 1, 1],
-                   [ 0, 1],
-                   [-1,-1]])
-    steps=arange(4)
-    aWin=False
-    for i in range(4):
-        for j in range(4):
-            row4=dir_key[i,0]*(steps-j)+row
-            col4=dir_key[i,1]*(steps-j)+col
-            if min((row4)>=0) & (max(row4)<nrow) & (min(col4)>=0) & (max(col4)<ncol):
-                #ind=col4+row4*ncol
-                ind=sub2ind(row4,col4,nrow,ncol)
-                if all(board[row4[:],col4[:]]==board[row,col]):
-                #if all(board.flatten()[ind]==board[row,col]):
-                    aWin=True
-                    return aWin, row4,col4
-    return aWin, [], []
+from calcWinner import calcWinner
 def pickMove(isXturn,board):
     openCol=(board[0,:]==0).nonzero()[0]
     if openCol.size==0:
         col=nan
         row=nan
     else:
-        col=random.choice(openCol)
+        one_move=copy(board)
+        weight=random.rand(openCol.size)
+        #weight based on board is a natural place to intoduce ML
+        if isXturn:
+            me=5
+            op=10
+        else:
+            me=10
+            op=5
+        for consider,col in enumerate(openCol):
+            row=sum(board[:,col]==0)-1
+            one_move[row,col]=me
+            aWin,win_row,win_col=calcWinner(one_move,row,col,nrow,ncol)
+            one_move[row,col]=0
+            if aWin:
+                return row,col
+        for consider,col in enumerate(openCol):
+            row=sum(board[:,col]==0)-1
+            one_move[row,col]=op
+            aWin,win_row,win_col=calcWinner(one_move,row,col,nrow,ncol)
+            one_move[row,col]=0
+            if aWin:
+                return row,col
+        bestMove=argmax(weight)
+        col=openCol[bestMove]
         row=sum(board[:,col]==0)-1
     return row,col
 
 #if __name__=='_main_':
+ion()
+plt_x=tile(atleast_2d(arange(ncol+1))  ,[nrow+1,1])-.5
+plt_y=tile(atleast_2d(arange(nrow+1)).T,[1,ncol+1])-.5
 nrow=6
 ncol=7
 isXturn=True
@@ -44,14 +48,27 @@ while playing:
     row,col=pickMove(isXturn,board)
     if isfinite(row):
         if isXturn:
-            board[row,col]=1
+            board[row,col]=5
+            win_val=4
         else:
-            board[row,col]=2
+            board[row,col]=10
+            win_val=9
         aWin,win_row,win_col=calcWinner(board,row,col,nrow,ncol)
         if aWin:
             playing=False
-            board[win_row[:],win_col[:]]=4
+            board[win_row[:],win_col[:]]=win_val
         else:
             isXturn=not(isXturn)
     else:
         playing=False # board is full
+    clf()
+    imshow(board,clim=[0,10])
+    plot(plt_x  ,plt_y  ,'-w')
+    plot(plt_x.T,plt_y.T,'-w')
+    pause(1)
+
+
+
+
+
+
